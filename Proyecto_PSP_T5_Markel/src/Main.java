a6ef1b86a6fb312f7d784d3a0702b8ea4518c68a
Markel <markel.fernandezdelabastida@ikasle.egibide.org>
2022-11-23 21:48:47 +0100

    Proyecto creado, gitignore creado

---
+++ Proyecto_PSP_T5_Markel/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.Serializable;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Cliente{
+
+
+
+}
+
+class Servidor{
+
+}
+
+
+class Hilo implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
+
+class Usuarios implements Serializable{
+
+}
