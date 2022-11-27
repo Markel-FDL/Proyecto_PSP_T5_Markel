0edb83487406785aad1738765979c7b7c5040c6c
Markel Fernandez de Labastida <markel.fernandezdelabastida@ikasle.egibide.org>
2022-11-27 16:47:50 +0100

    Aun mas dudas

--- Proyecto_PSP_T5_Markel/src/Main.java
+++ Proyecto_PSP_T5_Markel/src/Main.java
@@ -14,13 +14,13 @@ public class Main {
 }
 
 class Cliente {
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Seleccion();
     }
 
-    public void Seleccion() throws IOException {
+    public static void Seleccion() throws IOException, NoSuchAlgorithmException {
         int num = 0;
 
         Socket cliente = new Socket("localhost", 5553);
@@ -32,10 +32,10 @@ class Cliente {
                 System.out.println("Bienvenido a la banca online");
                 System.out.println("\n1. Iniciar sesión");
                 System.out.println("2. Registrarse");
-                System.out.println("Selecciona una opción: ");
+                System.out.println("Selecciona una opción (9 salir): ");
                 num = scanner.nextInt();
                 scanner.nextLine();
-                if (num < 0 || num > 2) {
+                if (num < 0 || num >= 3 || num != 9) {
                     System.out.println("Error al insertar dato");
                 }
             } while (num < 0 || num > 2);
@@ -45,14 +45,18 @@ class Cliente {
 
         if (num == 1) {
             enviar_dato.write(num);
+            Iniciar_sesion(cliente);
         } else if (num == 2) {
             enviar_dato.write(num);
+            Registrarse(cliente);
+        } else if (num == 9) {
+            System.exit(0);
         }
 
 
     }
 
-    public void Registrarse(Socket cliente) throws NoSuchAlgorithmException, IOException {
+    public static void Registrarse(Socket cliente) throws NoSuchAlgorithmException, IOException {
         String nombre;
         String apellido = null;
         int edad = 0;
@@ -148,15 +152,89 @@ class Cliente {
 
         Usuarios usuario = new Usuarios(nombre, apellido, edad, email, usuarioo, new String(resumen));
 
+        Usuarios usuarios = new Usuarios();
+
+        usuarios.escribir_usuario(usuario);
+
         enviar_objeto.writeObject(usuario);
 
+        System.out.println("Usuario creado");
+
+        Seleccion();
 
     }
+
+    public static void Iniciar_sesion(Socket cliente) throws NoSuchAlgorithmException, IOException {
+        String usuarioo = null;
+        String contrasena;
+
+        ObjectOutputStream enviar_objeto = new ObjectOutputStream(cliente.getOutputStream());
+
+        System.out.println("Inicio de sesión");
+
+        try {
+            do {
+                System.out.println("\nUsuario: ");
+                usuarioo = scanner.nextLine();
+                if (Objects.equals(usuarioo, "")) {
+                    System.out.println("No puedes dejar el usuario vacío");
+                }
+            } while (Objects.equals(usuarioo, ""));
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+
+        MessageDigest md = MessageDigest.getInstance("SHA");
+        byte[] resumen = new byte[0];
+        try {
+            do {
+                System.out.println("\nIntroduce tu contraseña: ");
+                contrasena = scanner.nextLine();
+                if (Objects.equals(contrasena, "")) {
+                    System.out.println("No puedes dejar el apellido vacío");
+                }
+            } while (Objects.equals(contrasena, ""));
+            byte[] contra = contrasena.getBytes();
+            md.update(contra);
+            resumen = md.digest();
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+
+        Usuarios usuario = new Usuarios(usuarioo, new String(resumen));
+
+        enviar_objeto.writeObject(usuario);
+
+        if ()
+
+    }
+
+
 
 }
 
 class Servidor{
     public static void main(String[] args) {
+
+    }
+
+    public static void Comprobar_normas_banco(Socket cliente) throws IOException {
+
+        ObjectInputStream outDato = new ObjectInputStream(cliente.getInputStream());
+
+        String contrato = "Normas del banco.";
+
+        DataOutputStream enviar_norma = new DataOutputStream(cliente.getOutputStream());
+        DataInputStream recivir_norma = new DataInputStream(cliente.getInputStream());
+
+        enviar_norma.writeUTF(contrato);
+
+        String respuesta = recivir_norma.readUTF();
+
+        if (respuesta == "s") {
+
+        }
+
 
     }
 
@@ -169,6 +247,17 @@ class Hilo implements Runnable{
     public void run() {
 
     }
+
+    public void Comprobar_inicio_sesion() throws IOException, ClassNotFoundException {
+        ObjectInputStream outDato = new ObjectInputStream(s.getInputStream());
+
+        Usuarios usuario = (Usuarios) outDato.readObject();
+
+
+
+
+    }
+
 }
 
 class Usuarios implements Serializable{
@@ -188,6 +277,14 @@ class Usuarios implements Serializable{
         this.usuario = usuario;
         this.contrasena = contrasena;
     }
+    public Usuarios(){
+
+    }
+    
+    public Usuarios(String usuario, String contrasena){
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
 
     public void Mostrar(){
         System.out.println("Nombre: " + nombre + " \nApellido: " + apellido + "\nEdad: " + edad + "\nEmail: " + email + "\nUsuario: " + usuario + "\ncontrasena: " + contrasena);
@@ -195,7 +292,7 @@ class Usuarios implements Serializable{
 
     public void escribir_usuario(Usuarios usuario) throws IOException {
         // ObjectOutputStream para poder escribir en el fichero binario de directores "Directores.dat". Si no existe, se crea automaticamente.
-        ObjectOutputStream escribir = new ObjectOutputStream(new FileOutputStream("Usuarios.dat"));
+        ObjectOutputStream escribir = new ObjectOutputStream(new FileOutputStream("Usuarios.txt"));
 
         escribir.writeObject(usuario);
 
